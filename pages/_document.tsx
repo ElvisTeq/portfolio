cf560864ea4b59cb3e52078708c997ea62f7a021
elvis <cenelvis@gmail.com>
2022-08-20 02:38:53 -0400

    asd

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+// Component to Override CSS
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html className="dark">
+      <Head></Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
